de9ce8415e0ad8dd15a92ec1da048059e4950fd0
Alex <alexpelletier2003@gmail.com>
2024-09-19 10:51:41 +0200

    refacto : fonction lecture fichier

--- src/entropie.rs
+++ src/entropie.rs
@@ -2,7 +2,16 @@ use std::collections::HashMap;
 use std::fs::File;
 use std::io::{BufReader, Read};
 
-pub fn comptage_lettres(file_path : String) -> HashMap<char, i32> {
+pub fn comptage_lettres(file_path : String) -> HashMap<String, i32> {
+    let (mut occurences, contents) = read_file(file_path);
+    for car in contents.chars() {
+        let stat = occurences.entry(car.to_string()).or_insert(0);
+        *stat += 1;
+    }
+    occurences
+}
+
+fn read_file(file_path: String) -> (HashMap<String, i32>, String) {
     let fichier = match File::open(file_path) {
         Ok(f) => {
             // L'ouverture du fichier s'est bien déroulée, on renvoie l'objet
@@ -16,20 +25,11 @@ pub fn comptage_lettres(file_path : String) -> HashMap<char, i32> {
         }
     };
 
-    let mut occurences:HashMap<char,i32> = HashMap::new();
+    let mut occurences: HashMap<String, i32> = HashMap::new();
     let mut buf_reader = BufReader::new(fichier);
     let mut contents = String::new();
     buf_reader.read_to_string(&mut contents).expect("Erreur dans la lecteur du fichier");
-
-    for car in contents.chars() {
-        let stat = occurences.entry(car).or_insert(0);
-        *stat += 1;
-    }
-
-    // nombre de caractères
-
-    occurences
-
+    (occurences, contents)
 }
 
 pub fn calcul_entropie(occurences: &mut Vec<i32>) -> f64 {
@@ -38,7 +38,6 @@ pub fn calcul_entropie(occurences: &mut Vec<i32>) -> f64 {
     let mut entropie: f64 = 0f64;
 
     println!("occurences : {:?}", occurences);
-
     for occ in occurences {
         let pi = *occ as f64 / n as f64;
         // println!("{}",pi);
@@ -48,26 +47,8 @@ pub fn calcul_entropie(occurences: &mut Vec<i32>) -> f64 {
 }
 
 pub fn comptage_mots(file_path : String) -> HashMap<String, i32> {
-    let fichier = match File::open(file_path) {
-        Ok(f) => {
-            // L'ouverture du fichier s'est bien déroulée, on renvoie l'objet
-            f
-        }
-        Err(e) => {
-            // Il y a eu un problème, affichons l'erreur pour voir ce qu'il se passe
-            println!("erreur : {:?}", e);
-            // On ne peut pas renvoyer le fichier ici, donc on quitte la fonction
-            panic!("Erreur dans la lecteur du fichier");
-        }
-    };
-
-    let mut occurences:HashMap<String,i32> = HashMap::new();
-    let mut buf_reader = BufReader::new(fichier);
-    let mut contents = String::new();
-    buf_reader.read_to_string(&mut contents).expect("Erreur dans la lecteur du fichier");
-
+    let (mut occurences, contents) = read_file(file_path);
     let mut mot = String::from("");
-
     for car in contents.chars() {
         match car {
             ' ' => push_word(&mut occurences, &mut mot),
@@ -78,9 +59,7 @@ pub fn comptage_mots(file_path : String) -> HashMap<String, i32> {
             c => mot.push(c)
         }
     }
-
     push_word(&mut occurences, &mut mot);
-
     occurences
 }
 
